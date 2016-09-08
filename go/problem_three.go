b80ee2a16bf3054a81b19cd41128edaf537ca885
Chris Stavitsky <cstavitsky@enova.com>
2016-09-08 15:27:55 -0500

    Refactor problem 3

--- go/problem_three.go
+++ go/problem_three.go
@@ -7,69 +7,53 @@ package main
 import "fmt"
 import "sort"
 
+// findFactors Gathers All Factors
 func findFactors(n int) []int {
   
   var factors []int
 
-  // Collect Factors
-  // Begin at n/47 because of number's huge size
-  if n == 600851475143 {
-    for i := n/47; i > 0 ; i-- {
-      if i % 2 == 0 || i % 3 == 0 || i % 5 == 0 || i % 7 == 0 || i % 11 == 0 || i % 13 == 0 || i % 17 == 0 || i % 19 == 0 || i % 23 == 0 || i % 29 == 0 {
-        continue
-      }
-      // If n is divisible by i, i is a Factor
-      if n % i == 0 && n > 1 {
-        factors = append(factors, i)
-      } else {
-        continue
-      }
-    }
-  } else {
-    // for smaller numbers
-    for i := n; i > 0 ; i-- {
-      if i % 2 == 0 || i % 3 == 0 || i % 5 == 0 || i % 7 == 0 || i % 11 == 0 || i % 13 == 0 || i % 17 == 0 || i % 19 == 0 || i % 23 == 0 || i % 29 == 0 {
-        continue
-      }
-      // If n is divisible by i, i is a Factor
-      if n % i == 0 && n > 1 {
-        factors = append(factors, i)
-      } else {
-        continue
-      }
+  for i := 1; i < n; i++ {
+    if n % i == 0 {
+      n = n/i
+      factors = append(factors, i)
+      factors = append(factors, n)
     }
   }
-
   return factors
 }
 
+// isPrime...
 func isPrime(n int) bool {
-  if len(findFactors(n)) == 2 {
-    fmt.Print(n, "IS PRIME")
-    return true
-  }
-  return false
+  return len(findFactors(n)) == 2
 }
 
+// Largest...
 func Largest(nums sort.IntSlice) int {
-  // sort.Sort(sort.Reverse(nums))
-  return nums[0]
+  largest := nums[0]
+
+  for _, n := range nums {
+    if n > largest {
+      largest = n
+    }
+  }
+
+  return largest
 }
 
 func main() {
-  
+  // Container for Prime Factors
   var primeFactors []int
 
-  factors := findFactors(600851475143)
+  // Gather All Factors
+  allFactors := findFactors(600851475143)
 
-  fmt.Println("Finding factors...")
-  for _, f := range factors {
+  // Select Prime Factors
+  for _, f := range allFactors {
     if isPrime(f) {
       primeFactors = append(primeFactors, f)
     }
   }
 
-  // largest := Largest(primeFactors)
-  fmt.Println("Prime Factors: ", primeFactors)
-  fmt.Println("Solution", primeFactors[0])
+  // Sort and Return Largest Prime Factor
+  fmt.Println("Largest prime factor: ", Largest(primeFactors))
 }
