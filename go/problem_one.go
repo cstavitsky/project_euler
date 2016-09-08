08e44d9335722186e65c5fd35d3edcf30c088b9d
Chris Stavitsky <cstavitsky@enova.com>
2016-09-08 14:52:56 -0500

    problem 1

--- go/problem_one.go
+++ go/problem_one.go
@@ -1,28 +1,28 @@
-package main 
+// If we list all the natural numbers below 10 that are multiples of
+// 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-import("fmt")
+// Find the sum of all the multiples of 3 or 5 below 1000.
 
-//Find the sum of all multiples of 3 and 5 below 1000
+package main
 
-func Add(nums []int) int {
-	var sum int
-	for _, i := range nums {
-		sum += i
-	}
-	return sum
-}
+import(
+  "fmt"
+)
 
-func FindMultiples() []int {
-	var multiples []int
-	for i := 0; i < 1000; i++ {
-		if i % 15 == 0 || i % 5 == 0 || i % 3 == 0 {
-			multiples = append(multiples, i)
-		}
-	}
-	return multiples
+func findMultiples(num int, factors ...int) map[int]bool {
+
+  // Gather all unique factors
+  allMultiples := make(map[int]bool)
+
+  for _, f := range factors {
+    if num % f == 0 && allMultiples[f] == false {
+      allMultiples[f] = true
+    }
+  }
+
+  return allMultiples
 }
 
 func main(){
-	sum := Add(FindMultiples())
-	fmt.Println(sum)
+  fmt.Print(findMultiples(10, 3, 5))
 }
